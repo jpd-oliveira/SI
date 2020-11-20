6e99e8444941dfc4caa42c6d5a78c071e03a4806
João Oliveira <jpd.oliveira@gmail.com>
2020-11-20 09:51:30 +0000

    added more dispatch

--- Supervisor/kbase/dispatcher.pl
+++ Supervisor/kbase/dispatcher.pl
@@ -19,3 +19,42 @@ defrule gotox_finish
         assert_once( action(stop_x)),
         retract(goto_x(Xf))
     ].
+
+defrule gotoy_inside
+    if goto_y(Yf) and y_is_at(Yi) and (Yi<Yf) and x_moving(0) and z_moving(0) and y_moving(0)
+    then [
+        assert_once( action(move_y_inside))
+    ].
+
+defrule gotoy_outside
+    if goto_y(Yf) and y_is_at(Yi) and (Yi>Yf) and x_moving(0) and z_moving(0) and y_moving(0)
+    then [
+        assert_once( action(move_y_outside))
+    ].
+
+defrule gotoz_finish
+    if goto_y(Yf) and y_is_at(Yf)
+    then [
+        assert_once( action(stop_y)),
+        retract(goto_y(Yf))
+    ].
+
+defrule gotoz_up
+    if goto_z(Zf) and z_is_at(Zi) and (Zi<Zf) and z_moving(0) and y_is_at(2) and y_moving(0)
+    then [
+        assert_once( action(move_z_up))
+    ].
+
+defrule gotoz_down
+    if goto_z(Zf) and z_is_at(Zi) and (Zi>Zf) and z_moving(0) and y_is_at(2) and y_moving(0)
+    then [
+        assert_once( action(move_z_down))
+    ].
+
+
+defrule gotoz_finish
+    if goto_z(Zf) and z_is_at(Zf)
+    then [
+        assert_once( action(stop_z)),
+        retract(goto_z(Zf))
+    ].
