a1c3c2a2955074c74843bf7e9c14f5a7f0aa77b6
João Oliveira <jpd.oliveira@gmail.com>
2020-11-28 09:58:46 +0000

    ensure load dispatcher

--- Supervisor/kbase/warehouse_planner.pl
+++ Supervisor/kbase/warehouse_planner.pl
@@ -1,4 +1,5 @@
 :-ensure_loaded('RTXEngine/RTXstrips_planner').
+:-ensure_loaded('dispatcher.pl').
 
 
 act goto_xz(Xf, Zf)
@@ -21,45 +22,4 @@ act put_in_cell(X,Z,Block)
        del  [cage(Block)]
        endcond[not(cage),y_is_at(2)].
 
-act goto_y(Yf)
-   pre  [x_is_at(Yi), (Yi\==Yf)]
-   add [x_is_at(Yf)]
-   del  [x_is_at(Yi)]
-   endcond[y_is_at(Yf)].
 
-
-act goto_z(Zf)
-   pre [z_is_at(Zi), (Zi\=Zf)]
-   add [z_is_at(Zf)]
-   del [z_is_at(Zi)]
-   endcond[z_is_at(Zf)].
-
-act take_part(X,Z)
-   pre [not(cage(Block)), x_is_at(X),z_is_at(Z), cell(X,Z,_)]
-   add [cage(Block)]
-   del [cell(X,Z,Block)]
-   endcond[cage(Block),y_is_at(2)].
-
-act put_in_leftS(Block)
-   pre [x_is_at(1),z_is_at(1),cage(Block)]
-   add [leftS(Block)]
-   del [cage(Block)]
-   endcond[not(cage),y_is_at(2)].
-
-act take_from_leftS(Block)
-   pre [x_is_at(1),z_is_at(1), not(cage(_))]
-   add [cage(Block)]
-   del []
-   endcond[].
-
-act put_in_rightS(Block)
-   pre [x_is_at(10),z_is_at(1),cage(Block)]
-   add [rightS(Block)]
-   del [cage(Block)]
-   endcond[not(cage),y_is_at(2)].
-
-act take_from_rightS(Block)
-   pre [x_is_at(10),z_is_at(1), not(cage(_))]
-   add [cage(Block)]
-   del []
-   endcond[].
